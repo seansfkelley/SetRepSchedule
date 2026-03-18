8daa104d10b7c757e9276eeb8216d3115bdceb50
Sean Kelley <seansfkelley@users.noreply.github.com>
2026-03-18 09:10:04 -0700

    Initial Commit

---
+++ SetRepSchedule/SetRepScheduleApp.swift
@@ -0,0 +1,12 @@
+//
+
+import SwiftUI
+
+@main
+struct SetRepScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetRepScheduleTests/SetRepScheduleTests.swift
@@ -0,0 +1,12 @@
+//
+
+import Testing
+@testable import SetRepSchedule
+
+struct SetRepScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
